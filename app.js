68f4ec9559827613c20ee6da9a16deabb456d43d
kacoro <kacoro@qq.com>
2019-06-15 05:37:57 +0800

    01koa第一讲环境搭建 以及第一个Koa实例

---
+++ app.js
@@ -0,0 +1,14 @@
+var Koa = require("koa")
+
+var app = new Koa()
+
+
+//配置路由
+
+//中间件
+app.use(async(ctx)=>{
+    ctx.body = '你好 koa2.x';
+})
+
+app.listen(3001)
+console.log('运行http://localhost:3001')
