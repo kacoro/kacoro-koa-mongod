34ad8f85376296ca9034674367dcc3eb51978008
kacoro <kacoro@qq.com>
2019-10-30 22:07:01 +0800

    根据路由分片

--- webapp/src/router/index.jsx
+++ webapp/src/router/index.jsx
@@ -1,44 +1,47 @@
 import React, { Component,Suspense, lazy  } from 'react'
 import { BrowserRouter as Router, Route, Redirect, Switch,withRouter } from "react-router-dom";
+import App from '@app/views/App'
 
+// import Loadable from 'react-loadable';
 
-// import asyncComponent from '@app/components/AsyncComponent'
+import asyncComponent from '@app/components/AsyncComponent'
 
-// const AsyncFirst = asyncComponent(() => import('@app/views/First'));
-// const AsyncSecond = asyncComponent(() => import('@app/views/Second'));
-// const AsyncThird = asyncComponent(() => import('@app/views/Third'));
-// const routes = [
-//   { path: '/', component: AsyncFirst },
-//   { path: '/first', component: AsyncFirst },
-//   { path: '/second', component: AsyncSecond },
-//   { path: '/third', component: AsyncThird }
-//   ];
+const AsyncFirst = asyncComponent(() => import(/* webpackChunkName: 'First' */ '@app/views/First'));
+const AsyncSecond = asyncComponent(() => import(/* webpackChunkName: 'Second' */ '@app/views/Second'));
+const AsyncThird = asyncComponent(() => import(/* webpackChunkName: 'Second' */ '@app/views/Third'));
+
 
 // const AsyncFirst = Loadable({
 //   loading: <div>loading...</div>,
-//   loader: () => import('@app/views/First'), 
+//   loader: () => import(/* webpackChunkName: 'First' */ '@app/views/First'), 
 //  })
  
 //  const AsyncSecond = Loadable({
 //   loading: <div>loading...</div>,
-//   loader: () => import('@app/views/Second'), 
+//   loader: () => import(/* webpackChunkName: 'Second' */ '@app/views/Second'), 
 //  })
 //  const AsyncThird = Loadable({
 //   loading: <div>loading...</div>,
-//   loader: () => import('@app/views/Third'), 
+//   loader: () => import(/* webpackChunkName: 'Third' */ '@app/views/Third'), 
 //  })
 
-import App from '@app/views/App'
-import First from '@app/views/First';
-import Second from '@app/views/Second';
-import Third from '@app/views/Third';
-
-const routes = [
-  { path: '/', component: withRouter(First) },
-  { path: '/first', component: withRouter(First) },
-  { path: '/second', component: withRouter(Second)},
-  { path: '/third', component: withRouter(Third) }
+ const routes = [
+  { path: '/', component: AsyncFirst },
+  { path: '/first', component: AsyncFirst },
+  { path: '/second', component: AsyncSecond },
+  { path: '/third', component: AsyncThird }
   ];
+
+// import First from '@app/views/First';
+// import Second from '@app/views/Second';
+// import Third from '@app/views/Third';
+
+// const routes = [
+//   { path: '/', component: withRouter(First) },
+//   { path: '/first', component: withRouter(First) },
+//   { path: '/second', component: withRouter(Second)},
+//   { path: '/third', component: withRouter(Third) }
+//   ];
 
 
 
