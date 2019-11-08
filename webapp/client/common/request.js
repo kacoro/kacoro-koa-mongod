937bfa2dfcee14bacff64e94daf55a5688eeadda
kacoro <kacoro@qq.com>
2019-11-09 03:41:38 +0800

    修改线上地址

--- webapp/client/common/request.js
+++ webapp/client/common/request.js
@@ -23,7 +23,7 @@ class Request {
     constructor() {
         this.base = {
             type: 'get',
-            meta: isNode ? 'http://127.0.0.1:5200' : 'http://127.0.0.1:5200'
+            meta: isNode ? 'https://www.kacoro.com' : 'http://127.0.0.1:5200'
         };
         this.options = {
             url: null,
